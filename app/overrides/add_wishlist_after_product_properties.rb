4a86de092d7a502cad0d0cd221d5c03aea545490
Waylon Peele <waylon@cloudcliques.com>
2014-12-17 13:53:57 -0800

    temporarily disabled deface overrides

--- app/overrides/add_wishlist_after_product_properties.rb
+++ app/overrides/add_wishlist_after_product_properties.rb
@@ -2,5 +2,6 @@ Deface::Override.new(
   :virtual_path => "spree/products/show",
   :name => "add_submit_wishlist_link",
   :insert_after => "[data-hook='cart_form']",
-  :partial => "spree/shared/wishlist"
+  :partial => "spree/shared/wishlist",
+  :disbaled => true
 )
--- app/overrides/add_wishlist_link_before_my_account.rb
+++ app/overrides/add_wishlist_link_before_my_account.rb
@@ -2,5 +2,6 @@ Deface::Override.new(
   :virtual_path => "spree/shared/_main_nav_bar",
   :name => "add_wishlist_link",
   :insert_after => "li#link-to-cart",
-  :partial => "spree/shared/wishlist_link"
+  :partial => "spree/shared/wishlist_link",
+  :disabled => true
 )
